7f9c4053e0149e10933cf1eb825f2a1a140a3b8e
Mino <mino.rakotolehibe@brighthr.com>
2025-11-07 14:39:51 +0000

    feat: Remove old tasks

--- TechTask/AppMain.swift
+++ TechTask/AppMain.swift
@@ -1,16 +1,12 @@
 //  Copyright © 2025 BrightHR. All rights reserved.
 
-import ComposableArchitecture
 import SwiftUI
 
 @main
 struct AppMain: App {
   var body: some Scene {
     WindowGroup {
-      Root(store: Store(
-        initialState: RootLogic.State(),
-        reducer: { RootLogic() }
-      ))
+      Text("Hello world")
     }
   }
 }
