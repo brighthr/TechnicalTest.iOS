3d54ecd427056a7b8859783dcf0d92dd0e000fcb
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2026-02-03 21:27:20 +0100

    Add environment status UI to ContentView

--- EnvironmentSettings/EnvironmentSettings/EnvironmentSettings/Views/ContentView.swift
+++ EnvironmentSettings/EnvironmentSettings/EnvironmentSettings/Views/ContentView.swift
@@ -10,10 +10,44 @@ import SwiftUI
 struct ContentView: View {
     @State private var viewModel: EnvironmentSettingsViewModel = EnvironmentSettingsViewModel()
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            VStack {
+                environmentStatusView
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+private extension ContentView {
+    var environmentStatusView: some View {
+        VStack(alignment: .leading, spacing: 22) {
+            HStack {
+                Text("Environment:")
+                    .foregroundStyle(.secondary)
+                Spacer()
+                Text(viewModel.state.selectedOAuthEnvironment.description)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.red)
+            }
+            
+            Divider()
+            
+            HStack {
+                Text("Feature Override")
+                    .foregroundStyle(.secondary)
+                Spacer()
+                Text(viewModel.state.isFeatureOverrideEnabled ? "Enabled" : "Disabled")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(viewModel.state.isFeatureOverrideEnabled  ? .green : .secondary)
+            }
+        }
+        .padding()
+        .background(.regularMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 14))
+        .padding()
+    }
 }
