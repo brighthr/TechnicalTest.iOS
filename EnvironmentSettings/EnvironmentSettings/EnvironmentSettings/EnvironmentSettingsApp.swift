38624833394abc42fd6f6f090df4e7f0bcd40b0c
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2026-02-03 21:50:53 +0100

    Add SwiftUI app entrypoint
    Create EnvironmentSettingsApp.swift defining the @main EnvironmentSettingsApp. The new file imports Foundation and SwiftUI and provides a WindowGroup that hosts ContentView, establishing the app entry point for the EnvironmentSettings target.

---
+++ EnvironmentSettings/EnvironmentSettings/EnvironmentSettings/EnvironmentSettingsApp.swift
@@ -0,0 +1,18 @@
+//
+//  EnvironmentSettingsApp.swift
+//  EnvironmentSettings
+//
+//  Created by Alex Trubacs on 03/02/2026.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct EnvironmentSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
